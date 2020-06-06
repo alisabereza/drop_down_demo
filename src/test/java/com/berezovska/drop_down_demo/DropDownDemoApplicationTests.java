fdbe48b5128907b90b929361fc226fb1740d0425
alisabereza <alisabereza@gmail.com>
2020-06-06 21:32:47 +0300

    jquery file replaced (different version)

---
+++ src/test/java/com/berezovska/drop_down_demo/DropDownDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berezovska.drop_down_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropDownDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
